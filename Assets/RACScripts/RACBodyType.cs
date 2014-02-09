225e6fac21e3a91f2dbbc37b97b4290fd4ad287a
james <jimmylee@hotmail.com>
2014-02-09 16:21:36 -0500

    Added all files to GIT. Created RAC versions of classes to extend and implement functionality later.

---
+++ Assets/RACScripts/RACBodyType.cs
@@ -0,0 +1,113 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RACBodyType : MonoBehaviour 
+{
+	public bool isMale = false;
+
+	[Range(0, 1)]
+	public float height = 0.5f;
+	[Range(0, 1)]
+	public float headSize = 0.5f;
+	[Range(0, 1)]
+	public float headWidth = 0.5f;
+	[Range(0, 1)]
+	public float neckThickness = 0.5f;
+	[Range(0, 1)]
+	public float armLength = 0.5f;
+	[Range(0, 1)]
+	public float forearmLength = 0.5f;
+	[Range(0, 1)]
+	public float armWidth = 0.5f;
+	[Range(0, 1)]
+	public float forearmWidth = 0.5f;
+	
+	[Range(0, 1)]
+	public float handsSize = 0.5f;
+	[Range(0, 1)]
+	public float feetSize = 0.5f;
+	[Range(0, 1)]
+	public float legSeparation = 0.5f;
+	[Range(0, 1)]
+	public float upperMuscle = 0.5f;
+	[Range(0, 1)]
+	public float lowerMuscle = 0.5f;
+	[Range(0, 1)]
+	public float upperWeight = 0.5f;
+	[Range(0, 1)]
+	public float lowerWeight = 0.5f;
+	[Range(0, 1)]
+	public float legsSize = 0.5f;
+	[Range(0, 1)]
+	public float belly = 0.5f;
+	[Range(0, 1)]
+	public float waist = 0.5f;
+	[Range(0, 1)]
+	public float gluteusSize = 0.5f;
+	
+	[Range(0, 1)]
+	public float earsSize = 0.5f;
+	[Range(0, 1)]
+	public float earsPosition = 0.5f;
+	[Range(0, 1)]
+	public float earsRotation = 0.5f;
+	[Range(0, 1)]
+	public float noseSize = 0.5f;
+	[Range(0, 1)]
+	public float noseCurve = 0.5f;
+	[Range(0, 1)]
+	public float noseWidth = 0.5f;
+	[Range(0, 1)]
+	public float noseInclination = 0.5f;
+	[Range(0, 1)]
+	public float nosePosition = 0.5f;
+	[Range(0, 1)]
+	public float nosePronounced = 0.5f;
+	[Range(0, 1)]
+	public float noseFlatten = 0.5f;
+
+	[Range(0, 1)]
+	public float chinSize = 0.5f;
+	[Range(0, 1)]
+	public float chinPronounced = 0.5f;
+	[Range(0, 1)]
+	public float chinPosition = 0.5f;
+
+	[Range(0, 1)]
+	public float mandibleSize = 0.5f;
+	[Range(0, 1)]
+	public float jawsSize = 0.5f;
+	[Range(0, 1)]
+	public float jawsPosition = 0.5f;
+
+	[Range(0, 1)]
+	public float cheekSize = 0.5f;
+	[Range(0, 1)]
+	public float cheekPosition = 0.5f;
+	[Range(0, 1)]
+	public float lowCheekPronounced = 0.5f;
+	[Range(0, 1)]
+	public float lowCheekPosition = 0.5f;
+	
+	[Range(0, 1)]
+	public float foreheadSize = 0.5f;
+	[Range(0, 1)]
+	public float foreheadPosition = 0.5f;
+	
+	[Range(0, 1)]
+	public float lipsSize = 0.5f;
+	[Range(0, 1)]
+	public float mouthSize = 0.5f;
+	[Range(0, 1)]
+	public float eyeRotation = 0.5f;
+	[Range(0, 1)]
+	public float eyeSize = 0.5f;
+
+	[Range(0, 1)]
+	public float breastSize = 0.5f;
+
+	//TODO add new data here
+	[Range(0, 1)]
+	public float shoulder = 0.0f;
+}
+
---
+++ Assets/RACScripts/RACCustomization.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class RACCustomization : UMACustomization 
+{
+	List<RACModifiableControl> modifiableControlList = new List<RACModifiableControl>();
+
+	// Use this for initialization
+	protected void Start () 
+	{
+		base.Start();
+	}
+}
